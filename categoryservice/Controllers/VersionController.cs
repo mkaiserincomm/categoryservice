c599500709ca9612a5c0ab6abc14958d3512d6f0
mkaiserincomm <mkaiser@incomm.com>
2021-09-30 14:29:18 -0400

    Update version controller

--- categoryservice/Controllers/VersionController.cs
+++ categoryservice/Controllers/VersionController.cs
@@ -7,13 +7,11 @@ namespace CategoryService.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class VersionController : ControllerBase
-    {
-        private readonly CategoryContext _context;
+    {        
         private readonly ILogger<VersionController> _logger;
 
-        public VersionController(ILogger<VersionController> logger, CategoryContext context)
-        {
-            _context = context;
+        public VersionController(ILogger<VersionController> logger)
+        {            
             _logger = logger;
             _logger.LogInformation(new EventId(1001,"Constructor"),"RedisControler VersionController");
         }
